b305381a0338b20c1610f193ae232ee96b2d5d7e
kholoudnour <kholoud.nour@hotmail.com>
2022-05-25 03:21:01 +0200

    CICD

---
+++ app.py
@@ -0,0 +1,56 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+DATABASE = os.path.join(PROJECT_ROOT, 'todo.db')
+
+app = Flask(__name__)
+# To run the project on your local machine.
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////" + DATABASE
+
+# to connect the app to RDS server
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://{user}:{password}@{rds-url}:{port}/{db-name}'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+
+
+@app.route("/")
+def index():
+    todo_list = Todo.query.all()
+    return render_template("index.html", todo_list=todo_list)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/complete/<string:todo_id>")
+def complete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/delete/<string:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True, host='0.0.0.0', port=3000)
---
+++ readme.md
@@ -0,0 +1,22 @@
+### Simple Flask Todo App
+
+#### How to run the project local
+<hr>
+
+- Create python3 virtual environment and activate it (python3 -m venv venv)
+- Run '. venv/bin/activate'
+- Run 'pip install -r requirements.txt' on terminal
+- Run 'python app.py' on terminal for start local server
+- To stop the server press Ctrl + C
+- To Exit from the virtual environment type 'deactivate' and press Enter
+
+<hr>
+
+#### Required Tasks
+
+[ ] Push the project to github
+[ ] make some changes on index.html, commit changes and push the changes to your repo.
+[ ] connect the project to AWS RDS Server
+[ ] write the app dockerfile
+[ ] push the image to ECR
+[ ] building and pushing the image using CircleCI
